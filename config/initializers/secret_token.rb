72443ab20ea8b8a67d3787014d43c016bc66aa9e
Patrycja Urban <patka@MoustacheMac.local>
2011-04-30 19:01:47 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wypozyczalnia::Application.config.secret_token = '81c871e5f52e523fc0368f4f4ca7f69d73f359c87676676c4e4af185970e05a26c9af641f135a154a98934156174e34c4c9105a1122ce28da5e7a96ec29c4e57'
